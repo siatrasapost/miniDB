2a7e1a751b8d3905c64870ab9ec3e6921ea33695
siatrasapost <siatrasapost@gmail.com>
2021-02-28 02:24:32 +0200

    left_outer_join created

--- database.py
+++ database.py
@@ -387,6 +387,62 @@ class Database:
             else:
                 res.show()
 
+    def full_outer(self, left_table_name, right_table_name, condition, save_as=None, return_object=False):
+        '''
+        Full Outer Join two tables that are part of the database where condition is met.
+        left_table_name -> left table's name (needs to exist in database)
+        right_table_name -> right table's name (needs to exist in database)
+        condition -> a condition using the following format :
+                    'column[<,<=,==,>=,>]value' or
+                    'value[<,<=,==,>=,>]column'.
+
+                    operatores supported -> (<,<=,==,>=,>)
+        save_as -> The name that will be used to save the resulting table in the database. Def: None (no save)
+        return_object -> If true, the result will be a table object (usefull for internal usage). Def: False (the result will be printed)
+        '''
+        self.load(self.savedir)
+        if self.is_locked(left_table_name) or self.is_locked(right_table_name):
+            print(f'Table/Tables are currently locked')
+            return
+
+        res = self.tables[left_table_name]._full_outer(self.tables[right_table_name], condition)
+        if save_as is not None:
+            res._name = save_as
+            self.table_from_object(res)
+        else:
+            if return_object:
+                return res
+            else:
+                res.show()
+
+    def left_outer(self, left_table_name, right_table_name, condition, save_as=None, return_object=False):
+        '''
+        Left Outer Join two tables that are part of the database where condition is met.
+        left_table_name -> left table's name (needs to exist in database)
+        right_table_name -> right table's name (needs to exist in database)
+        condition -> a condition using the following format :
+                    'column[<,<=,==,>=,>]value' or
+                    'value[<,<=,==,>=,>]column'.
+
+                    operatores supported -> (<,<=,==,>=,>)
+        save_as -> The name that will be used to save the resulting table in the database. Def: None (no save)
+        return_object -> If true, the result will be a table object (usefull for internal usage). Def: False (the result will be printed)
+        '''
+        self.load(self.savedir)
+        if self.is_locked(left_table_name) or self.is_locked(right_table_name):
+            print(f'Table/Tables are currently locked')
+            return
+
+        res = self.tables[left_table_name]._left_outer(self.tables[right_table_name], condition)
+        if save_as is not None:
+            res._name = save_as
+            self.table_from_object(res)
+        else:
+            if return_object:
+                return res
+            else:
+                res.show()
+
     def lockX_table(self, table_name):
         '''
         Locks the specified table using the exclusive lock (X)
--- table.py
+++ table.py
@@ -328,6 +328,111 @@ class Table:
 
         return join_table
 
+    def _full_outer(self, table_right: Table, condition):
+        '''
+        Full Outer Join table (left) with a supplied table (right) where condition is met.
+        '''
+        # get columns and operator
+        column_name_left, operator, column_name_right = self._parse_condition(condition, join=True)
+
+        # try to find both columns, if you fail raise error
+        try:
+            column_index_left = self.column_names.index(column_name_left)
+            column_index_right = table_right.column_names.index(column_name_right)
+        except:
+            raise Exception(f'Columns dont exist in one or both tables.')
+
+        # get the column names of both tables with the table name in front
+        # ex. for left -> name becomes left_table_name_name etc
+        left_names = [f'{self._name}_{colname}' for colname in self.column_names]
+        right_names = [f'{table_right._name}_{colname}' for colname in table_right.column_names]
+
+        # define the new tables name, its column names and types
+        join_table_name = f'{self._name}_full-outer-join_{table_right._name}'
+        join_table_colnames = left_names+right_names
+        join_table_coltypes = self.column_types+table_right.column_types
+        join_table = Table(name=join_table_name, column_names=join_table_colnames, column_types= join_table_coltypes)
+
+        # count the number of operations (<,> etc)
+        no_of_ops = 0
+
+        # this code is dumb on purpose... it needs to illustrate the underline technique
+        # for each value in left column and right column, if condition, append the corresponding row to the new table
+        for row_left in self.data:
+            left_value = row_left[column_index_left]
+            for row_right in table_right.data:
+                right_value = row_right[column_index_right]
+                no_of_ops+=1
+                if get_op(operator, left_value, right_value): #EQ_OP
+                    join_table._insert(row_left+row_right)
+
+        print(f'## Select ops no. -> {no_of_ops}')
+        print(f'# Left table size -> {len(self.data)}')
+        print(f'# Right table size -> {len(table_right.data)}')
+
+        return join_table
+
+    def _left_outer(self, table_right: Table, condition):
+        '''
+        Left Outer Join table (left) with a supplied table (right) where condition is met.
+        '''
+        # get columns and operator
+        column_name_left, operator, column_name_right = self._parse_condition(condition, join=True)
+
+        # try to find both columns, if you fail raise error
+        try:
+            column_index_left = self.column_names.index(column_name_left)
+            column_index_right = table_right.column_names.index(column_name_right)
+        except:
+            raise Exception(f'Columns dont exist in one or both tables.')
+
+        # get the column names of both tables with the table name in front
+        # ex. for left -> name becomes left_table_name_name etc
+        left_names = [f'{self._name}_{colname}' for colname in self.column_names]
+        right_names = [f'{table_right._name}_{colname}' for colname in table_right.column_names]
+
+        # define the new tables name, its column names and types
+        join_table_name = f'{self._name}_left-outer-join_{table_right._name}'
+        join_table_colnames = left_names+right_names
+        join_table_coltypes = self.column_types+table_right.column_types
+        join_table = Table(name=join_table_name, column_names=join_table_colnames, column_types= join_table_coltypes)
+
+        # count the number of operations (<,> etc)
+        no_of_ops = 0
+        
+
+        # this code is dumb on purpose... it needs to illustrate the underline technique
+        # for each value in left column and right column, if condition, append the corresponding row to the new table
+        for row_left in self.data:
+            counter = 0
+            null_list=[]
+            left_value = row_left[column_index_left]
+            temp=[]
+            for row_right in table_right.data:
+                right_value = row_right[column_index_right]
+                no_of_ops+=1
+                if get_op('==', left_value, right_value): #EQ_OP
+                    temp.append([True, row_left+row_right])
+                else:
+                    temp.append([False, row_left])
+
+            for i in range (len(temp)):
+                if temp[i][0] is False:
+                    counter+=1           
+            if counter==len(table_right.data):
+                for i in range (table_right._no_of_columns):
+                    null_list.append(0)
+                join_table._insert(row_left+null_list)
+            else:
+                for i in range (len(temp)):
+                    if temp[i][0] is True:
+                        join_table._insert(temp[i][1])
+
+        print(f'## Select ops no. -> {no_of_ops}')
+        print(f'# Left table size -> {len(self.data)}')
+        print(f'# Right table size -> {len(table_right.data)}')
+
+        return join_table
 
     def show(self, no_of_rows=None, is_locked=False):
         '''
@@ -348,8 +453,7 @@ class Table:
         # if we dont skip these rows, the returning table has empty rows at the deleted positions
         non_none_rows = [row for row in self.data if any(row)]
         # print using tabulate
-        print(tabulate(non_none_rows[:no_of_rows], headers=headers)+'\n')
-
+        print(tabulate(non_none_rows[:no_of_rows], headers=headers)+'\n')  
 
     def _parse_condition(self, condition, join=False):
         '''
