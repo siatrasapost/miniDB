8cc01f256c68175f9ff9b07ccdac955b52e1b28d
siatrasapost <siatrasapost@gmail.com>
2021-02-28 02:36:56 +0200

    removed some comments

--- joins.py
+++ joins.py
@@ -3,8 +3,6 @@ from database import Database
 db = Database('smdb', load=True)
 
 def full_outer(left_table, right_table, column_left_table, column_right_table, condition = '=='):
-    #db.inner_join(left_table, right_table,'dept_name==dept_name')
-    #db.inner_join(left_table, right_table,column_left_table+condition+column_right_table)
     db.full_outer(left_table, right_table, column_left_table+condition+column_right_table)
 
 def left_outer(left_table, right_table, column_left_table, column_right_table, condition = '=='):
